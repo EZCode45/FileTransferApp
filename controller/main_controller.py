1e6df7d0080c93fda70c6a2ef60ce7e3ef14f9e0
EZCode45 <eishan.hinduja@gmail.com>
2025-03-17 19:59:14 -0400

    touch ups

--- controller/main_controller.py
+++ controller/main_controller.py
@@ -4,5 +4,15 @@ from view.main_view import MainView
 class MainController():
     def __init__(self, view: MainView):
         self.view = view
-        devices = ["0.0.0.0", "192.10.0.2"]
-        MainView.update_ip_list(self.view, devices)
+        self.devices = ["0.0.0.0", "192.10.0.2"]
+        #step 1 of button bind: add callback trough controller
+        self.view.add_callbacks("upload", self.upload)
+        self.view.add_callbacks("refresh", self.refresh)
+        #step 2 bind commands
+        self.view.bind_commands()
+    def upload(self):
+        self.view.request_file()
+    def refresh(self):
+        self.view.update_ip_list(self.devices)
+
+    
--- view/main_view.py
+++ view/main_view.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import filedialog
 #fg = text color
 #bg = background color
 #font = font style and size
@@ -9,26 +10,46 @@ class MainView():
         self.root = root
         self.root.geometry("300x500")
         self.root.title("File Sharing APP")
-
+        self.callbacks = dict()
         self.title = tk.Label(root,
                           text="File Sharing APP",
                           fg="Blue", bg="white",
                           font=("Arial", 20))
         self.title.pack(padx="2")
         self.ip_list = tk.Listbox(root,
-                            bg="cyan",
+                            bg="#137fad",
                             fg = "white",
-                            font=("Arial", 15),
-                            height=5, width=50)
+                            font=("Bookman Old Style", 15, "bold"),
+                            height=5, width=50,
+                            relief=tk.FLAT)
         self.ip_list.pack(pady="10")
         self.upload =  tk.Button(root,
                             text="Upload",
                             fg="white",
-                            bg="blue",
-                            font=("Arial", 15))
+                            bg="#137fad",
+                            font=("Bookman Old Style", 15, "bold"),
+                            relief=tk.FLAT)
         self.upload.pack(pady="100")
+        self.refresh = tk.Button(root,
+                            text="Refresh",
+                            fg="white",
+                            bg="#137fad",
+                            font=("Bookman Old Style", 15, "bold"),
+                            relief=tk.FLAT,
+                            width=7,
+                            height=1)
+        self.refresh.pack(pady="5")
     def update_ip_list(self, devices):
         # update device ip list  with the new devices
+        self.ip_list.delete(0, tk.END)
         self.ip_list
         for device in devices:
             self.ip_list.insert(tk.END, device)
+    def request_file(self):
+        return filedialog.askopenfilename() # type: ignore
+    def add_callbacks(self, key, method):
+        self.callbacks[key] = method
+    def bind_commands(self):
+        # bind the commands to the buttons
+        self.upload.config(command=self.callbacks["upload"])
+        self.refresh.config(command=self.callbacks["refresh"])
