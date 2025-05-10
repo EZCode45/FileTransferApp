5166fa738dee3d40c5ae5653da18efcf34857db2
EZCode45 <eishan.hinduja@gmail.com>
2025-05-10 13:05:50 -0400

    added som ftp stuff

--- run.py
+++ run.py
@@ -4,10 +4,12 @@ from view.main_view import MainView
 from controller.main_controller import MainController
 from ftplib import FTP as ftp
 # Initialize FTP instance
-from model.services.server import run
-ftp_server = run()  # Ensure no null bytes or encoding issues
-ftp_instance = ftp.__init__(ftp_server)
+from model.services.server import run  # Ensure no null bytes or encoding issues
+ftp_instance = ftp()
+ftp_instance.connect('localhost', 21)
+ftp_instance.login('admin', 'uhvhgjbvdjbhgoefijrwpjnbheori2pjebfgvfyr98u34ijrefbgyv99rihtgy58u9rihgvy78furhvgyiufrhqgvfdysi8dgetuh3gy8fuhbvhdwiqueohjvfdijwbavhfwri3kejfbvjsdpockjbhvuiforjfbvhjxciodajhfvgtu9ri0fjvhbsdfiesojhvbhgkrfijdhbvjicojhbdkgjiofjvbdjiapofjhbgjogifjvhhsfiu9ghif0sdiuchvgfur9efhdvdicuuhguifdhvjcudfshgfiodbadfuwhbfhjdkjcbfhdjkjhvbfjdkcnabvngjlfgmbjdl;vkbejfkwnvjbghitjorfjkvbghifjkbhgrjbghrfjvbhgfjhbghrjfbhjfvhfjvbh')
 print(ftp_instance)
+# ftp_instance.connect()
 selected_ip = None
 file_path = None
 if __name__ == "__main__":
