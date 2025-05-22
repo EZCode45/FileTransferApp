61fb626841074d72586faf73512e246f3b2d2830
EZCode45 <eishan.hinduja@gmail.com>
2025-05-22 19:30:11 -0400

    got it to work

--- run.py
+++ run.py
@@ -6,8 +6,8 @@ from ftplib import FTP as ftp
 # Initialize FTP instance
 from model.services.server import run  # Ensure no null bytes or encoding issues
 ftp_instance = ftp()
-ftp_instance.connect('10.1.10.109', 21)
-ftp_instance.login('admin', 'uhvhgjbvdjbhgoefijrwpjnbheori2pjebfgvfyr98u34ijrefbgyv99rihtgy58u9rihgvy78furhvgyiufrhqgvfdysi8dgetuh3gy8fuhbvhdwiqueohjvfdijwbavhfwri3kejfbvjsdpockjbhvuiforjfbvhjxciodajhfvgtu9ri0fjvhbsdfiesojhvbhgkrfijdhbvjicojhbdkgjiofjvbdjiapofjhbgjogifjvhhsfiu9ghif0sdiuchvgfur9efhdvdicuuhguifdhvjcudfshgfiodbadfuwhbfhjdkjcbfhdjkjhvbfjdkcnabvngjlfgmbjdl;vkbejfkwnvjbghitjorfjkvbghifjkbhgrjbghrfjvbhgfjhbghrjfbhjfvhfjvbh')
+ftp_instance.connect('127.0.0.1', 21)
+ftp_instance.login('admin', 'uhvhgjbvdjbhERTYUIO%WE&*@#%#$%^&*@#@^$#@%^&**UGCR%^&*@TRDHUYREW$^&SDFGHJ^%$#$%^*UHHJG&YGFGUTDgoefijrwpjnbheori2pjebfgvfyr98u34w5yeuwperfd6789dyft5u7398rf8yugu90v8yfght4riueoh^REW$#%^&ITYFDSRE%$^&TFGSE%&TIGFGSRE%^&*YGFSE^%&*YUGFDER*OIHGCFG^*OYIHJGCDIYJC^&@#UIHJG%^@*&UIGFDE$&%*YUHGCXDFE^&*UGHCFGR^@&*YUGHDR*YUGF%^*YUGFDRE%^YUGFHDR&*YIUGJFTYR%&^*@&UIHGYFR&4ijrefbgyv99rihtgy58u9rihgvy78furhvgyiufrhqgvfdysi8dgeSETYOIHGCFDR^T&Y*OIHGDTYR^&@#UIHJGCDRT*U@POJHFGR^@&#*POJKBVGCHT*@UOPLIJHGCDR^@*&#PIOJLHKGDR^*&UIHJVR^%&*@UIGFDRT&*UOIHKGD^*@&OUIHKGD%^&*@tuh3gy8fuhbvhdwiqueohjvfdijwbavhfwri3kejfbvjsdpockjbhvuiforjfbvhjxciodajhfvgtu9ri0fjvhbsdfiesojhvbhgkrfijdhbvjicojhbdkgjiofjvbdjiapofjhbgjogifjvhhsfiu9ghif0sdiuchvgfur9efhdvdicuuhguifdhvjcudfshgfiodbadfuwhbfhjdkjcbfhdjkjhvbfjdkcnabvngjlfgmbjdl;vkbejfkwnvjbghitjorfjkvbghifjkbhgrjbghrfjvbhgfjhbghrjfbhjfvhfjvbh')
 # print(ftp_instance)
 # ftp_instance.connect()
 run()
