f64d7c21896ed4cc6cb1ef3bcb5bfa1e606b8ae4
EZCode45 <eishan.hinduja@gmail.com>
2025-05-24 13:56:48 -0400

    made code connect to the ftp server

--- run.py
+++ run.py
@@ -7,12 +7,13 @@ from ftplib import FTP as ftp
 server_ip = '127.0.0.1'
 server_port = 21
 from model.services.server import run  # Ensure no null bytes or encoding issues
+run()
 ftp_instance = ftp()
 ftp_instance.connect('127.0.0.1', 21)
-ftp_instance.login('admin', 'uhvhgjbvdjbhERTYUIO%WE&*@#%#$%^&*@#@^$#@%^&**UGCR%^&*@TRDHUYREW$^&SDFGHJ^%$#$%^*UHHJG&YGFGUTDgoefijrwpjnbheori2pjebfgvfyr98u34w5yeuwperfd6789dyft5u7398rf8yugu90v8yfght4riueoh^REW$#%^&ITYFDSRE%$^&TFGSE%&TIGFGSRE%^&*YGFSE^%&*YUGFDER*OIHGCFG^*OYIHJGCDIYJC^&@#UIHJG%^@*&UIGFDE$&%*YUHGCXDFE^&*UGHCFGR^@&*YUGHDR*YUGF%^*YUGFDRE%^YUGFHDR&*YIUGJFTYR%&^*@&UIHGYFR&4ijrefbgyv99rihtgy58u9rihgvy78furhvgyiufrhqgvfdysi8dgeSETYOIHGCFDR^T&Y*OIHGDTYR^&@#UIHJGCDRT*U@POJHFGR^@&#*POJKBVGCHT*@UOPLIJHGCDR^@*&#PIOJLHKGDR^*&UIHJVR^%&*@UIGFDRT&*UOIHKGD^*@&OUIHKGD%^&*@tuh3gy8fuhbvhdwiqueohjvfdijwbavhfwri3kejfbvjsdpockjbhvuiforjfbvhjxciodajhfvgtu9ri0fjvhbsdfiesojhvbhgkrfijdhbvjicojhbdkgjiofjvbdjiapofjhbgjogifjvhhsfiu9ghif0sdiuchvgfur9efhdvdicuuhguifdhvjcudfshgfiodbadfuwhbfhjdkjcbfhdjkjhvbfjdkcnabvngjlfgmbjdl;vkbejfkwnvjbghitjorfjkvbghifjkbhgrjbghrfjvbhgfjhbghrjfbhjfvhfjvbh')
+# ftp_instance.login('admin', 'uhvhgjbvdjbhERTYUIO%WE&*@#%#$%^&*@#@^$#@%^&**UGCR%^&*@TRDHUYREW$^&SDFGHJ^%$#$%^*UHHJG&YGFGUTDgoefijrwpjnbheori2pjebfgvfyr98u34w5yeuwperfd6789dyft5u7398rf8yugu90v8yfght4riueoh^REW$#%^&ITYFDSRE%$^&TFGSE%&TIGFGSRE%^&*YGFSE^%&*YUGFDER*OIHGCFG^*OYIHJGCDIYJC^&@#UIHJG%^@*&UIGFDE$&%*YUHGCXDFE^&*UGHCFGR^@&*YUGHDR*YUGF%^*YUGFDRE%^YUGFHDR&*YIUGJFTYR%&^*@&UIHGYFR&4ijrefbgyv99rihtgy58u9rihgvy78furhvgyiufrhqgvfdysi8dgeSETYOIHGCFDR^T&Y*OIHGDTYR^&@#UIHJGCDRT*U@POJHFGR^@&#*POJKBVGCHT*@UOPLIJHGCDR^@*&#PIOJLHKGDR^*&UIHJVR^%&*@UIGFDRT&*UOIHKGD^*@&OUIHKGD%^&*@tuh3gy8fuhbvhdwiqueohjvfdijwbavhfwri3kejfbvjsdpockjbhvuiforjfbvhjxciodajhfvgtu9ri0fjvhbsdfiesojhvbhgkrfijdhbvjicojhbdkgjiofjvbdjiapofjhbgjogifjvhhsfiu9ghif0sdiuchvgfur9efhdvdicuuhguifdhvjcudfshgfiodbadfuwhbfhjdkjcbfhdjkjhvbfjdkcnabvngjlfgmbjdl;vkbejfkwnvjbghitjorfjkvbghifjkbhgrjbghrfjvbhgfjhbghrjfbhjfvhfjvbh')
 # print(ftp_instance)
 # ftp_instance.connect()
-run()
+# run()
 selected_ip = None
 file_path = None
 if __name__ == "__main__":
