699f12f1d6f15d6ba3389ab209f4417414345fad
EZCode45 <eishan.hinduja@gmail.com>
2025-05-29 20:00:28 -0400

    added part of frontend

--- run.py
+++ run.py
@@ -2,21 +2,21 @@ import tkinter as tk
 from model.main_model import MainModel
 from view.main_view import MainView
 from controller.main_controller import MainController
-from ftplib import FTP as ftp
+from ftplib import FTP
 # Initialize FTP instance
 server_ip = '127.0.0.1'
 server_port = 21
 from model.services.server import run  # Ensure no null bytes or encoding issues
 run()
-ftp_instance = ftp()
-ftp_instance.connect('127.0.0.1', 21)
-# ftp_instance.login('admin', 'uhvhgjbvdjbhERTYUIO%WE&*@#%#$%^&*@#@^$#@%^&**UGCR%^&*@TRDHUYREW$^&SDFGHJ^%$#$%^*UHHJG&YGFGUTDgoefijrwpjnbheori2pjebfgvfyr98u34w5yeuwperfd6789dyft5u7398rf8yugu90v8yfght4riueoh^REW$#%^&ITYFDSRE%$^&TFGSE%&TIGFGSRE%^&*YGFSE^%&*YUGFDER*OIHGCFG^*OYIHJGCDIYJC^&@#UIHJG%^@*&UIGFDE$&%*YUHGCXDFE^&*UGHCFGR^@&*YUGHDR*YUGF%^*YUGFDRE%^YUGFHDR&*YIUGJFTYR%&^*@&UIHGYFR&4ijrefbgyv99rihtgy58u9rihgvy78furhvgyiufrhqgvfdysi8dgeSETYOIHGCFDR^T&Y*OIHGDTYR^&@#UIHJGCDRT*U@POJHFGR^@&#*POJKBVGCHT*@UOPLIJHGCDR^@*&#PIOJLHKGDR^*&UIHJVR^%&*@UIGFDRT&*UOIHKGD^*@&OUIHKGD%^&*@tuh3gy8fuhbvhdwiqueohjvfdijwbavhfwri3kejfbvjsdpockjbhvuiforjfbvhjxciodajhfvgtu9ri0fjvhbsdfiesojhvbhgkrfijdhbvjicojhbdkgjiofjvbdjiapofjhbgjogifjvhhsfiu9ghif0sdiuchvgfur9efhdvdicuuhguifdhvjcudfshgfiodbadfuwhbfhjdkjcbfhdjkjhvbfjdkcnabvngjlfgmbjdl;vkbejfkwnvjbghitjorfjkvbghifjkbhgrjbghrfjvbhgfjhbghrjfbhjfvhfjvbh')
-# print(ftp_instance)
-# ftp_instance.connect()
-# run()
+ftp = FTP(server_ip, server_port)
+ftp.connect(server_ip, server_port)
+ftp.retrlines('LIST')  # List files in the FTP server root directory
 selected_ip = None
 file_path = None
 if __name__ == "__main__":
+    with open('HELLO' ,'wb') as fp:
+        ftp.retrbinary('RETR HELLO', fp.write)
     root = tk.Tk()
-    controller = MainController(MainModel(selected_ip, file_path, ftp_instance), MainView(root))
+    controller = MainController(MainModel(selected_ip, file_path, ftp), MainView(root))
     root.mainloop()
+    ftp.quit()  # Ensure the FTP connection is closed when the application exits
