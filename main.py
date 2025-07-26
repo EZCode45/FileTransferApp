ce0c52149d1783b3275aab7f238e0dd19ec303e0
EZCode45 <eishan.hinduja@gmail.com>
2025-07-26 15:04:20 -0400

    stuff

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
 
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+templates = Jinja2Templates(directory="/")
+app.mount("/", StaticFiles(directory="/"))
+
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def read_root(request: Request):
+    return templates.TemplateResponse("mainscreen.html", context = {"request": request})
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
