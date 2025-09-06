90a766a9342ac9d37b56327774408ccfdb2e8b0f
EZCode45 <eishan.hinduja@gmail.com>
2025-09-06 12:59:11 -0400

    Replace index.html with mainpage.html and update button handling in main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 import tkinter as tk
 from tkinter import filedialog as fd
+import sys
 # from starlette.responses import FileResponse 
 
 app = FastAPI()
@@ -22,15 +23,18 @@ app.mount("/static", StaticFiles(directory="static"))
 
 @app.get("/")
 def read_root(request: Request):
-    return templates.TemplateResponse('index.html', {"request": request})
+    return templates.TemplateResponse('mainpage.html', {"request": request})
+@app.get("/api/get_ip")
+def get_connected_ips(request: Request):
+    print(request.client)
 
-
+get_connected_ips(Request)
 @app.post("/button-clicked")
 async def handle_button_click(request: Request, action: str = Form(...)):
     global file_path
     if action == "transferbuttonclicked":
         file_path = fd.askopenfilename()
-        return templates.TemplateResponse("/index.html", {"request": request, "message": "opened filedialog"})
+        return templates.TemplateResponse("/mainpage.html", {"request": request, "message": "opened filedialog"})
     return HTMLResponse("<h1>Unknown action</h1>")
 
 
